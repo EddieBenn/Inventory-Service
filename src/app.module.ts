da4ebaeafa27b9eb71da671770c1673d1a7f1095
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-12-04 08:27:00 +0100

    fixes made, project running successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InventoryModule } from './inventory/inventory.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    InventoryModule,
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
